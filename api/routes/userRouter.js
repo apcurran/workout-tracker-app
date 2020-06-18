6d9bf19cfe05c5dfba534cc460d5cccd6a6bdef9
apc <alexcurran.va@gmail.com>
2020-06-18 15:51:54 -0500

    Base outline for userRouter complete

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+const db = require("../../db/index");
+const bcrypt = require("bcrypt");
+
+router.post("/register", async (req, res) => {
+    res.send("Registering account...");
+});
+
+router.post("/login", async (req, res) => {
+    res.send("Registering account...");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const PORT = process.env.PORT || 5000;
 // Import Routes
 const workoutRouter = require("./api/routes/workoutRouter");
+const userRouter = require("./api/routes/userRouter");
 
 const app = express();
 
@@ -18,5 +19,6 @@ app.use(express.json());
 
 // Routers
 app.use("/api/workout", workoutRouter);
+app.use("/user", userRouter);
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode and Listening on port, ${PORT}.`));
