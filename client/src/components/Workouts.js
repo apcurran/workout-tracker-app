22bc3dc9e3705af207404f996234fe5fd741e316
apc <alexcurran.va@gmail.com>
2020-06-23 17:47:07 -0500

    Base layout setup for Workouts page

--- client/src/components/Workouts.js
+++ client/src/components/Workouts.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import WorkoutsList from "./WorkoutsList";
 
 export default function Workouts() {
     return (
-        <div>
-            Workouts Page
+        <div className="workouts-grid-container">
+            <WorkoutsList />
+            <div className="stripes"></div>
         </div>
     )
 }
---
+++ client/src/components/WorkoutsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function WorkoutsList() {
+    return (
+        <section className="workouts-list">
+            Workouts List Section
+        </section>
+    )
+}
