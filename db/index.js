20d59c4f265754cdd35828fea3d2737ec5a06329
apc <alexcurran.va@gmail.com>
2020-07-01 10:16:11 -0500

    Updated db to include Heroku config

--- db/index.js
+++ db/index.js
@@ -1,12 +1,23 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
+let pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
 });
+
+if (process.env.NODE_ENV === "production") {
+    pool = new Pool(
+        {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+    );
+}
 
 module.exports = {
     query: (text, parameters) => pool.query(text, parameters)
